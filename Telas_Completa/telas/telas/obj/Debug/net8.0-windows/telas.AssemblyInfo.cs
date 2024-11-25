d620458de0f59d15f78c5a8352417b86574a88f6
Carlos Eduardo <cadulopes2007@gmail.com>
2024-11-25 12:32:28 -0300

    .

--- Telas_Completa/telas/telas/obj/Debug/net8.0-windows/telas.AssemblyInfo.cs
+++ Telas_Completa/telas/telas/obj/Debug/net8.0-windows/telas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("telas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a395ac771af820f9e331c5dc24139c7c006bc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc82e0535bdabc1640f4a29d6ceb79cdae96f222")]
 [assembly: System.Reflection.AssemblyProductAttribute("telas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("telas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
